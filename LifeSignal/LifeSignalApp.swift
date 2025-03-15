7a50f5f9e0c4a59125d8dd9b7f198185634b5c90
Yunxin119 <1999yunxin@gmail.com>
2025-03-15 13:04:54 -0700

    Initial Commit

---
+++ LifeSignal/LifeSignalApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeSignalApp.swift
+//  LifeSignal
+//
+//  Created by Yunxin Liu on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct LifeSignalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
